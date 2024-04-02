863261042bf42492ec3dc0cce0034b1d30b10e45
prdsScndGitHub <165675049+prdsScndGitHub@users.noreply.github.com>
2024-04-02 17:26:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const startBtnEl = document.getElementById('start-btn');
-const audio = new Audio('img/hbdAudio.mp3'); // Assuming 'audiobd.mp3' is in the same folder as your JavaScript file
+const audio = new Audio('img/hbdAudio.mp3');
 
 startBtnEl.addEventListener('click', function (){
-    // Play the sound
+
     audio.play();
 
-    // Hide the button
-    startBtnEl.style.display = "none";
 
-    // Add a delay before showing the image
-    setTimeout(function() {
-        document.body.innerHTML = ""; // Clears the entire body
-        document.body.style.backgroundImage = "url('img/hbd-main2.gif')"; // Changes background image
-        document.body.style.backgroundSize = "99%"; // Makes the background image cover the entire body
-        document.body.style.backgroundPosition = "center"; // Centers the background image
-        document.body.style.backgroundRepeat = "no-repeat"; // Prevents the background image from repeating
-    }, 1000); // Change the delay time as needed (1000ms = 1 second)
+    startBtnEl.style.transition = "opacity 1s ease";
+    startBtnEl.style.opacity = "0";
 
     setTimeout(function() {
-        alert('Wishing you a happy birthday, May all your dreams and wishes come true!. I love you always <3')
+        startBtnEl.style.display = "none";
+    }, 1000); 
+
+
+    setTimeout(function() {
+        document.body.style.transition = "background-image 2s ease";
+        document.body.style.backgroundImage = "url('img/hbd-main2.gif')"; 
+        document.body.style.backgroundSize = "cover"; 
+        document.body.style.backgroundPosition = "center"; 
+        document.body.style.backgroundRepeat = "no-repeat"; 
+    }, 1500); 
+
+    setTimeout(function() {
+        alert('Wishing you a happy 21st birthday, May all your dreams and wishes come true!. I love you always <3')
     }, 16000);
 });
 
-// Adjust background size for smaller screens (e.g., mobile phones)
+
 window.addEventListener('resize', function() {
     if (window.innerWidth <= 768) {
         document.body.style.backgroundSize = "contain";
